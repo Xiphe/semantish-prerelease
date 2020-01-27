a9c7bc49931fb1aafe2328694df3706e07d93ddd
Xiphe <github@xiphe.net>
2020-01-27 23:52:01 +0100

    feat: go with the flow

--- release.config.js
+++ release.config.js
@@ -1,35 +1,13 @@
 const { branch } = require('env-ci');
-const releaseNpm = require('@semantic-release/npm');
 
 const DEFAULT_BRANCH = 'master';
-
-function toBetaVersion(origin) {
-  return (config, context) => {
-    return origin(config, {
-      ...context,
-      nextRelease: {
-        ...context.nextRelease,
-        channel: branch,
-        version: `${
-          context.nextRelease.version
-        }-beta.${context.nextRelease.gitHead.substring(0, 7)}`,
-      },
-    });
-  };
-}
-
-const npmPrerelease = {
-  ...releaseNpm,
-  prepare: toBetaVersion(releaseNpm.prepare),
-  release: toBetaVersion(releaseNpm.release),
-};
 
 module.exports = {
   branches: [DEFAULT_BRANCH, { name: '*', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    branch === DEFAULT_BRANCH ? '@semantic-release/npm' : npmPrerelease,
+    '@semantic-release/npm',
     branch === DEFAULT_BRANCH && '@semantic-release/github',
   ].filter(Boolean),
 };
