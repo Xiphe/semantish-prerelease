0c665d8c782042ce9183922baa11c6974f10209b
Xiphe <github@xiphe.net>
2021-11-16 22:17:16 +0100

    Revert "chore: debug"
    This reverts commit f8b7b6e9099e17e1bd51a6141884c58603eda959.

--- scripts/notifyPr.js
+++ scripts/notifyPr.js
@@ -1,7 +1,6 @@
 const { name, version } = require('../package.json');
 
 module.exports = () => {
-  console.log('NOTIFYING ABOUT ', { name, version });
   if (version && version !== '0.0.0-development' && version.match(/-/)) {
     return `<hr /><code>npm install ${name}@${version}</code>`;
   }
