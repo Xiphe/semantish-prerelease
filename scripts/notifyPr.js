f8b7b6e9099e17e1bd51a6141884c58603eda959
Xiphe <github@xiphe.net>
2021-11-16 22:15:39 +0100

    chore: debug

--- scripts/notifyPr.js
+++ scripts/notifyPr.js
@@ -1,6 +1,7 @@
 const { name, version } = require('../package.json');
 
 module.exports = () => {
+  console.log('NOTIFYING ABOUT ', { name, version });
   if (version && version !== '0.0.0-development' && version.match(/-/)) {
     return `<hr /><code>npm install ${name}@${version}</code>`;
   }
