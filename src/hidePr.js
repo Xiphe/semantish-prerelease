97e00a34ae92b5e732da9146d2de889e3c5c0869
Xiphe <github@xiphe.net>
2022-08-03 17:40:11 +0200

    fix: use GITHUB_HEAD_REF as branch for github pull_requests

--- src/hidePr.js
+++ src/hidePr.js
@@ -3,16 +3,33 @@ const getEnvCi = require('env-ci');
 module.exports = function hidePr(releasePr) {
   return (subContext) => {
     const envCi = getEnvCi(subContext);
-    if (!releasePr || !envCi.pr) {
+    if (
+      !envCi.isCi ||
+      !releasePr ||
+      envCi.name === 'Bamboo' ||
+      envCi.name === 'Bitbucket Pipelines' ||
+      envCi.name === 'AWS CodeBuild' ||
+      envCi.name === 'Codeship' ||
+      envCi.name === 'GitLab CI/CD' ||
+      envCi.name === 'TeamCity' ||
+      envCi.name === 'Jenkins' ||
+      envCi.name === 'Wercker' ||
+      !envCi.pr
+    ) {
       return envCi;
     }
+
+    const branch =
+      envCi.name === 'GitHub Actions'
+        ? process.env.GITHUB_HEAD_REF
+        : envCi.branch || envCi.prBranch;
 
     return {
       ...envCi,
       isPr: false,
       pr: undefined,
       prBranch: undefined,
-      branch: envCi.branch || envCi.prBranch,
+      branch,
     };
   };
 };
