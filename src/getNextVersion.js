ce8f11c888c58b44d57a9653ada3eaadf04bd2dd
Xiphe <github@xiphe.net>
2022-08-03 11:14:54 +0200

    feat: use timestamped pre-releases instead of using git hash
    this keeps releases in order
    
    BREAKING CHANGE:
    the format of pre-releases is different now

--- src/getNextVersion.js
+++ src/getNextVersion.js
@@ -1,17 +1,16 @@
 const getNextVersionOrigin = require('semantic-release/lib/get-next-version');
 
 module.exports = function getNextVersion({ logger, nextRelease, ...rest }) {
-  const { gitHead } = nextRelease;
-  const gitHeadToVersion = (v) =>
-    v.replace(/\.[0-9]+$/, `.${gitHead.substring(0, 7)}`);
+  const now = Date.now();
+  const timestampToVersion = (v) => v.replace(/\.[0-9]+$/, `.${now}`);
   const version = getNextVersionOrigin({
     ...rest,
     nextRelease,
     logger: {
       ...logger,
-      log: (...args) => logger.log(...args.map(gitHeadToVersion)),
+      log: (...args) => logger.log(...args.map(timestampToVersion)),
     },
   });
 
-  return gitHeadToVersion(version);
+  return timestampToVersion(version);
 };
