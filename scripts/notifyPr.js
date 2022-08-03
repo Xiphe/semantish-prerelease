399d5b871cc917a4cc13f08e3f496484b530d2bd
Xiphe <github@xiphe.net>
2022-08-03 12:08:16 +0200

    ci: use semantic-release plugin of notify pr

--- scripts/notifyPr.js
+++
@@ -1,8 +0,0 @@
-const { name, version } = require('../package.json');
-
-module.exports = () => {
-  if (version && version !== '0.0.0-development' && version.match(/-/)) {
-    return `<hr /><code>npm install ${name}@${version}</code>`;
-  }
-  return undefined;
-};
