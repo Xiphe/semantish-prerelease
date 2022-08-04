a1d80168600c47838b1a02cf7bb55a2503d3d2b4
Xiphe <github@xiphe.net>
2022-08-04 11:34:24 +0200

    feat: use human readable timestamp for versions

--- src/getNextVersion.js
+++ src/getNextVersion.js
@@ -1,8 +1,17 @@
 const getNextVersionOrigin = require('semantic-release/lib/get-next-version');
 
 module.exports = function getNextVersion({ logger, nextRelease, ...rest }) {
-  const now = Date.now();
-  const timestampToVersion = (v) => v.replace(/\.[0-9]+$/, `.${now}`);
+  const now = new Date();
+  const humanReadableTimestamp = `${String(now.getFullYear()).substring(
+    2,
+  )}${String(now.getMonth() + 1).padStart(2, '0')}${String(
+    now.getDate(),
+  ).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(
+    now.getMinutes(),
+  ).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
+
+  const timestampToVersion = (v) =>
+    v.replace(/\.[0-9]+$/, `.${humanReadableTimestamp}`);
   const version = getNextVersionOrigin({
     ...rest,
     nextRelease,
